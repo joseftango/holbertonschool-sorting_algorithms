844544dbe2ff5a9180a9610efe7a25f8b8eca81e
joseftango <4279@holbertonschool.com>
2022-09-21 11:06:07 +0100

    0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+/**
+* bubble_sort - function that arrange array to ascending order
+* @array: array
+* @size: size of the array
+* Return: void
+**/
+
+void bubble_sort(int *array, size_t size)
+{
+unsigned int i, j, k, flag, tmp = 0;
+
+for (i = 0; i < size - 1; i++)
+{
+flag = 0;
+
+for (j = 0; j < size - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+tmp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = tmp;
+flag = 1;
+k = 0;
+while (k < size)
+{
+if (k > 0)
+{
+printf(", ");
+}
+
+printf("%d", array[k]);
+k++;
+}
+putchar('\n');
+}
+
+}
+
+if (flag == 0)
+break;
+}
+
+}
