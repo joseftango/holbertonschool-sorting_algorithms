0598b3658e064a3afb0463e47c8d2d8fda5db239
joseftango <4279@holbertonschool.com>
2022-09-27 00:06:50 +0100

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,88 +1,70 @@
 #include "sort.h"
 /**
-* swap - function that swaps two integer
-* @a: pointer to integer
-* @b: pointer to integer
-* Return: void
-**/
-
-void swap(int *a, int *b)
-{
-int tmp = 0;
-
-tmp = *a;
-*a = *b;
-*b = tmp;
-}
-
-/**
-* partition - function that applicates Lomuto partition scheme
-* @a: array
-* @start: first index
-* @end: last index
-* @size: size of array
-* Return: index of pivot
-**/
-
-int partition(int *a, int start, int end, size_t size)
-{
-int pivot = a[end], partition_i = start, i;
-
-for (i = start; i < end ; i++)
-{
-if (a[i] <= pivot)
-{
-swap(&a[i], &a[partition_i]);
-partition_i++;
-print_array(a, size);
-}
-
-}
-
-swap(&a[partition_i], &a[end]);
-print_array(a, size);
-
-return (partition_i);
-}
-
-/**
-* sort - function that sorts an array
-* @a: array
-* @start: first index
-* @end: last index
-* @size: length of array
-* Return: void
-**/
-
-void sort(int *a, int start, int end, size_t size)
-{
-int pivot_i = 0;
-
-if (start >= end)
-return;
-
-pivot_i = partition(a, start, end, size);
-
-sort(a, start, pivot_i - 1, size);
-
-sort(a, pivot_i + 1, end, size);
-
-}
-
-/**
-* quick_sort - initial function
+* partition - Lomuto partition scheme
+*
 * @array: array to sort
+* @low: smallest index
+* @high: highest index
 * @size: array size
-* Return: void
+* Return: next index
 */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot;
+	int i, j, tmp = 0;
 
+	pivot = array[high];
+	i = low;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			if (tmp != array[i])
+				print_array(array, size);
+			i++;
+		}
+	}
+	tmp = array[i];
+	array[i] = array[high];
+	array[high] = tmp;
+	if (tmp != array[i])
+		print_array(array, size);
+	return (i);
+}
+/**
+ * sort - the sorting algorithm
+ *
+ * @array: array to sort
+ * @low: lowest index
+ * @high: highest index
+ * @size: array size
+ */
+void sort(int *array, int low, int high, size_t size)
+{
+
+	int pivot_index;
+
+	if (low >= high)
+		return;
+
+	pivot_index = partition(array, low, high, size);
+	sort(array, low, pivot_index - 1, size);
+	sort(array, pivot_index + 1, high, size);
+}
+/**
+ * quick_sort - initial function
+ *
+ * @array: array to sort
+ * @size: array size
+ */
 void quick_sort(int *array, size_t size)
 {
-unsigned int l = 0, r = size - 1;
-
-if (array == NULL || size < 2)
-return;
-
-sort(array, l, r, size);
-
+	if (size < 2 || array == NULL)
+		return;
+	sort(array, 0, size - 1, size);
 }
